0f928969566a1593128423a54940ffb3ec13480d
Thorgar <nstommel@outlook.de>
2013-12-02 22:06:42 +0100

    Initial commit
    Initial MVC5 Project

---
+++ Futura/Futura/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Futura.Startup))]
+namespace Futura
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
